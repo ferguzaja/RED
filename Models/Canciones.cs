5df1e1b1dbba31266723b6235b67db020a284cbd
Rulo Pimentel <rulopimentel@gmail.com>
2018-03-12 08:04:31 -0600

    Adecuación para nuestro BackEnd de easypaid. Se agrega método de Login y método para buscar la información del usuario

--- Models/Canciones.cs
+++
@@ -1,8 +0,0 @@
-namespace WebServer.Models {
-    public class VideoMusical {
-        public int ID { get; set; }
-        public string Nombre { get; set; }
-        public string Artista { get; set; }
-        public string Vistas { get; set; }
-    }
-}
---
+++ Models/Datos.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+
+namespace WebServer.Models
+{
+    public class Datos
+    {
+
+        /*
+         IDictionary Representa una colección genérica de pares clave-valor.
+         En este caso se crea un IDictionary de claves de tipo int, y valor de tipo Usuario
+         */
+        public static IDictionary<int, Usuario> Usuarios { get; private set; }
+
+        // Constructor de la clase
+        static Datos()
+        {
+            /*
+            Se agregan tres entradas de clave-valor a nuestro IDictionary
+             */
+            Usuarios = new Dictionary<int, Usuario>();
+            Usuarios.Add(0, new Usuario { ID = 0, Nombre = "Vlad", Apellidos = "Drăculea", Email = "alucard@gmail.com", Password = "123456" });
+            Usuarios.Add(1, new Usuario { ID = 1, Nombre = "Dorian", Apellidos = "Gray", Email = "eternalLife@gmail.com", Password = "23435643" });
+            Usuarios.Add(2, new Usuario { ID = 2, Nombre = "Jeanne", Apellidos = "d'Arc", Email = "DoncelladeOrleans@gmail.com", Password = "asdf445asd" });
+        }
+    }
+}
